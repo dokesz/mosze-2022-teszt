bbdf1b3231b513ed258a0c2646d996c312c6c8d4
dokesz <dokesz.uzleti@gmail.com>
2022-09-11 13:28:26 +0200

    fixing variable

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // elgepelt valtozo
+    int *b = new int[N_ELEMENTS]; 
     std::cout << '1-100 ertekek duplazasa'
     for (int i = 0;)   // felig kesz for ciklus
     {
