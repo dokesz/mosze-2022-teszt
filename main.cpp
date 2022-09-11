a53d3353560832d3fdd23c7127f4f42f9e390c90
dokesz <dokesz.uzleti@gmail.com>
2022-09-11 13:30:10 +0200

    fixing for cycle

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 int main()
 {
     int *b = new int[N_ELEMENTS]; 
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)   // felig kesz for ciklus
+    std::cout << '1-100 ertekek duplazasa';
+    for (int i = 0; i < N_ELEMENTS + 1; i++)   
     {
         b[i] = i * 2;
     }
