fd3ae69c0a775f3d3d5899d09b8f23adff459483
dokesz <dokesz.uzleti@gmail.com>
2022-09-11 13:20:15 +0200

    kommentek

--- main.cpp
+++ main.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 
-constexpr int N_ELEMENTS = 100;
+constexpr int N_ELEMENTS = 100; // #define hasznalata inkabb 
 
 int main()
 {
-    int *b = new int[NELEMENTS];
+    int *b = new int[NELEMENTS]; // elgepelt valtozo
     std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    for (int i = 0;)   // felig kesz for ciklus
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"   // hianyzo ; es ertek 
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++)  // , hasznalata ; helyett
     {
         atlag += b[i]
     }
